bb4b9aba332d787c3a8272eaceab005d9437c29d
 Marco Figueroa <marcofigueroa23@icloud.com>
2023-05-29 23:41:09 -0400

    fix: Order by Desc

--- src/services/accounts.service.js
+++ src/services/accounts.service.js
@@ -4,7 +4,7 @@ import database from '../database.js'
 class AccountService {
 
     async GetAccounts(){
-        const GetAccounts = await database.query(`SELECT id, name, email FROM dedsec.accounts;`)
+        const GetAccounts = await database.query(`SELECT id, name, email FROM dedsec.accounts ORDER BY id DESC;`)
         return GetAccounts
     }
 
